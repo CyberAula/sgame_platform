e48fc4bebf587315fdb11d184c3383693661e37d
agordillo <agordillos@gmail.com>
2017-08-30 14:30:20 +0200

    Demo links and minor fix

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -4,5 +4,9 @@ class DemoController < ApplicationController
 	def demo
 		@game_templates = GameTemplate.where(:certified => true)
 		@scormfiles = Scormfile.where(:certified => true)
+		if user_signed_in?
+			@game_templates = (@game_templates + current_user.game_templates).uniq
+			@scormfiles = (@scormfiles + current_user.scormfiles).uniq
+		end
 	end
 end
